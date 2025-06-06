51d5e27a9e5f341d90edb568d63884d86ac4d7d9
Aditi Bindal <aditii.b6@gmail.com>
2025-06-06 21:53:42 +0530

    Add restart in runner.sh

--- runner.sh
+++ runner.sh
@@ -6,6 +6,6 @@ systemctl daemon-reexec
 systemctl daemon-reload
 
 systemctl enable opensorus.service
-systemctl start opensorus.service
+systemctl restart opensorus.service
 
 systemctl status opensorus.service
